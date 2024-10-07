c45b36c38cadb6b91229c810c96006ecd273f356
Krishna Kumar D <dkrishnakumar987@gmail.com>
2024-10-07 12:11:33 +0530

    [Add] Added Login and SignUp Pages

---
+++ frontend/src/pages/login/Login.jsx
@@ -0,0 +1,56 @@
+const Login = () => {
+  return (
+    <div className="mx-auto flex min-w-96 flex-col items-center justify-center">
+      <div className="h-full w-full p-4 bg-gray-100 rounded-md bg-clip-padding backdrop-filter backdrop-blur-md bg-opacity-10 border border-gray-100">
+        <h1 className="text-center text-3xl font-semibold text-gray-300">
+          <span className="text-blue-500">ChatScape</span>
+        </h1>
+        <h1 className="text-center text-2xl font-semibold text-gray-300">
+          <span>Login</span>
+        </h1>
+        <form>
+          <div>
+            <label className="label p-2">
+              <span className="text-base label-text text-accent">
+                Username
+              </span>
+            </label>
+            <input
+              type="text"
+              placeholder="Enter Username"
+              className="w-full input input-bordered input-primary h-10"
+            ></input>
+          </div>
+
+          <div>
+            <label className="label">
+              <span className="text-base label-text text-accent">
+                Password
+              </span>
+            </label>
+            <input
+              type="password"
+              placeholder="Enter Password"
+              className="w-full input input-bordered input-primary h-10"
+            ></input>
+          </div>
+
+          <a
+            href="#"
+            className="text-sm hover:underline hover:text-blue-600 mt-2 inline-block text-primary"
+          >
+            {"Don't"} have an Account?
+          </a>
+
+          <div>
+            <button className="btn btn-block btn-outline btn-primary btn-sm mt-2">
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
